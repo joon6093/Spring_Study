a8ab87d617dbb0e65efba687b5c0d9b1a8ae26b5
Jeyong <joon6093@naver.com>
2024-02-25 22:08:51 +0900

    [ADD] gateway 서버 GlobalFilter를 적용하여 prefilter 및 postfilter 구현

--- apigateway-service/src/main/java/com/sjy/apigatewayservice/config/FilterConfig.java
+++ apigateway-service/src/main/java/com/sjy/apigatewayservice/config/FilterConfig.java
@@ -8,23 +8,23 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class FilterConfig {
     private final CustomFilter customFilter;
-    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-            .route(r -> r.path("/first-service/**")
-                .filters(f -> f.filter(customFilter.apply(new CustomFilter.Config()))
-                        .addRequestHeader("first-request", "first-request-header-by-java")
-                        .addResponseHeader("first-response", "first-response-header-by-java"))
-                .uri("http://127.0.0.1:8081"))
-            .route(r -> r.path("/second-service/**")
-                .filters(f -> f.filter(customFilter.apply(new CustomFilter.Config()))
-                        .addRequestHeader("second-request", "second-request-header-by-java")
-                        .addResponseHeader("second-response", "second-response-header-by-java"))
-                .uri("http://127.0.0.1:8082"))
-            .build();
-    }
+//    @Bean
+//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//            .route(r -> r.path("/first-service/**")
+//                .filters(f -> f.filter(customFilter.apply(new CustomFilter.Config()))
+//                        .addRequestHeader("first-request", "first-request-header-by-java")
+//                        .addResponseHeader("first-response", "first-response-header-by-java"))
+//                .uri("http://127.0.0.1:8081"))
+//            .route(r -> r.path("/second-service/**")
+//                .filters(f -> f.filter(customFilter.apply(new CustomFilter.Config()))
+//                        .addRequestHeader("second-request", "second-request-header-by-java")
+//                        .addResponseHeader("second-response", "second-response-header-by-java"))
+//                .uri("http://127.0.0.1:8082"))
+//            .build();
+//    }
 }
