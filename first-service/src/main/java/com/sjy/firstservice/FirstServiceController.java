6b19cbda19ae0be54163ea3a8bd608b4c4bf3b7a
Jeyong <joon6093@naver.com>
2024-02-25 21:46:16 +0900

    [ADD] gateway 서버 CustomFilter를 적용하여 prefilter 및 postfilter 구현

--- first-service/src/main/java/com/sjy/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/sjy/firstservice/FirstServiceController.java
@@ -1,6 +1,9 @@
 package com.sjy.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +21,9 @@ public class FirstServiceController {
     public String message(@RequestHeader("first-request") String header) {
         log.info(header);
         return "Hello World in First Service.";
+    }
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there. This is a message from First Service.";
     }
 }
