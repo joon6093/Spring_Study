cdf5238583225da7b8501e5857a6df47a83e477c
jeyong <joon6093@naver.com>
2024-03-04 04:58:01 +0900

    [ADD] user 서버에서 Feign Logging 설정을 추가하여 OpenFeign을 이용한 통신 로그를 볼 수 있도록 구현

--- user-service/src/main/java/com/sjy/userservice/client/OrderServiceClient.java
+++ user-service/src/main/java/com/sjy/userservice/client/OrderServiceClient.java
@@ -1,5 +1,6 @@
 package com.sjy.userservice.client;
 
+import com.sjy.userservice.config.FeignConfig;
 import com.sjy.userservice.dto.ResponseOrder;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name="ORDER-SERVICE")
+@FeignClient(name="ORDER-SERVICE", configuration = FeignConfig.class)
 public interface OrderServiceClient {
 
     @GetMapping("/order-service/{userId}/orders")
