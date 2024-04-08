2c7427c8153e6f762516bf9e37913e859cc423e2
jeyong <joon6093@naver.com>
2024-04-09 01:07:29 +0900

    init: : 프로젝트 환경 구축

---
+++ Spring-Batch_Study/src/test/java/com/sjy/springbatch_study/SpringBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjy.springbatch_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
