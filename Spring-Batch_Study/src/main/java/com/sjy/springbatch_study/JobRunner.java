3658a878559ece69baa4d5baed606ab86ea152fe
jeyong <joon6093@naver.com>
2024-04-09 21:24:07 +0900

    feat : jobParameters 사용해보기

---
+++ Spring-Batch_Study/src/main/java/com/sjy/springbatch_study/JobRunner.java
@@ -0,0 +1,30 @@
+package com.sjy.springbatch_study;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JobRunner implements ApplicationRunner {
+
+    private final JobLauncher jobLauncher;
+
+    private final Job job;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", "user3")
+                .addString("test", "user3")
+                .toJobParameters();
+
+        jobLauncher.run(job,jobParameters);
+    }
+}
