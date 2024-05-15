d6158957b375c72245c792fd82e342ad8acf7e05
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-05-15 17:47:27 +0900

    feat : repository에서 BooleanExpression을 이용한 동적 쿼리 생성

--- Querydsl_Study/src/test/java/study/querydsl/MemberJpaRepositoryTest.java
+++ Querydsl_Study/src/test/java/study/querydsl/MemberJpaRepositoryTest.java
@@ -53,7 +53,7 @@ class MemberJpaRepositoryTest {
     }
 
     @Test
-    public void searchTest() {
+    public void searchTestByBuilder() {
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -73,6 +73,30 @@ class MemberJpaRepositoryTest {
         condition.setAgeLoe(40);
         condition.setTeamName("teamB");
         List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        assertThat(result).extracting("username").containsExactly("member4");
+    }
+
+    @Test
+    public void searchTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();  // 검색은 페이징쿼리도 같이 해야힘
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
         assertThat(result).extracting("username").containsExactly("member4");
     }
 }
