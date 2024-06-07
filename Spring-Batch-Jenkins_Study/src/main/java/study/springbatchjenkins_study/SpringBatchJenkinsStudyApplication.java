b5bbfd443c996b31034b8c7320b2f865198dba9e
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-06-07 10:25:04 +0900

    init : 프로젝트 초기 환경 구축

---
+++ Spring-Batch-Jenkins_Study/src/main/java/study/springbatchjenkins_study/SpringBatchJenkinsStudyApplication.java
@@ -0,0 +1,13 @@
+package study.springbatchjenkins_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchJenkinsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchJenkinsStudyApplication.class, args);
+    }
+
+}
---
+++ Spring-Batch-Jenkins_Study/src/test/java/study/springbatchjenkins_study/SpringBatchJenkinsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springbatchjenkins_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchJenkinsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
