8204446688ed32c90e16be68f4001fddb50762c4
jeyong <joon6093@naver.com>
2024-06-09 23:55:21 +0900

    init : 초기 프로젝트 환경 구축

---
+++ ELK-Logging_Study/src/main/java/study/elklogging_study/ElkLoggingStudyApplication.java
@@ -0,0 +1,13 @@
+package study.elklogging_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkLoggingStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElkLoggingStudyApplication.class, args);
+    }
+
+}
