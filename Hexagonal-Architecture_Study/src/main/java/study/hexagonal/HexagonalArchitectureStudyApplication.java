fdac99e97b1d1e31ec7d21e2b7767823ee76ceab
jeyong <joon6093@naver.com>
2024-08-04 03:08:44 +0900

    init : 헥사고날 아키텍처 공부 프로젝트 생성
    https://github.com/wikibook/clean-architecture

---
+++ Hexagonal-Architecture_Study/src/main/java/study/hexagonal/HexagonalArchitectureStudyApplication.java
@@ -0,0 +1,13 @@
+package study.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalArchitectureStudyApplication.class, args);
+    }
+
+}
---
+++ Hexagonal-Architecture_Study/src/test/java/study/hexagonal/HexagonalArchitectureStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
