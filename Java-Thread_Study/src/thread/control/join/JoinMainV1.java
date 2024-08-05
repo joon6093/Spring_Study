e2701ad4de6cfb8fca0f7b52b22bc64d745b75e2
jeyong <joon6093@naver.com>
2024-08-05 23:09:44 +0900

    refactor : 폴더 구조 변경

--- Java-Thread_Study/src/thread/control/join/JoinMainV1.java
+++ Java-Thread_Study/src/thread/control/join/JoinMainV1.java
@@ -1,11 +1,11 @@
-package thread.join;
+package thread.control.join;
 
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
-public class JoinMainV3 {
+public class   JoinMainV1 {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         log("Start");
         SumTask task1 = new SumTask(1, 50);
         SumTask task2 = new SumTask(51, 100);
@@ -14,13 +14,6 @@ public class JoinMainV3 {
 
         thread1.start();
         thread2.start();
-
-        // 스레드가 종료될 때 까지 대기
-        log("join() - main 스레드가 thread1, thread2 종료까지 대기");
-        thread1.join();
-        thread2.join();
-        log("main 스레드 대기 완료");
-
         log("task1.result = " + task1.result);
         log("task2.result = " + task2.result);
 
--- Java-Thread_Study/src/thread/control/join/JoinMainV2.java
+++ Java-Thread_Study/src/thread/control/join/JoinMainV2.java
@@ -1,9 +1,9 @@
-package thread.join;
+package thread.control.join;
 
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
-public class   JoinMainV1 {
+public class JoinMainV2 {
 
     public static void main(String[] args) {
         log("Start");
@@ -14,6 +14,12 @@ public class   JoinMainV1 {
 
         thread1.start();
         thread2.start();
+
+        // 정확한 타이밍을 맞추어 기다리기 어려움
+        log("main 스레드 sleep()");
+        sleep(3000);
+        log("main 스레드 깨어남");
+
         log("task1.result = " + task1.result);
         log("task2.result = " + task2.result);
 
