97ec4923f6670fba245ef549b0bbb55095cbe5d2
jeyong <joon6093@naver.com>
2024-08-14 07:40:04 +0900

    feat : @annotation 문법 사용하기

---
+++ Spring-Aop_Study/src/test/java/study/aop/pointcut/AtAnnotationTest.java
@@ -0,0 +1,39 @@
+package study.aop.pointcut;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import study.aop.member.MemberService;
+import study.aop.pointcut.AtAnnotationTest.AtAnnotationAspect;
+
+@Slf4j
+@Import(AtAnnotationAspect.class)
+@SpringBootTest
+public class AtAnnotationTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy={}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class AtAnnotationAspect {
+
+        @Around("@annotation(study.aop.member.annotation.MethodAop)")
+        public Object doAtAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[@annotation] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+    }
+}
