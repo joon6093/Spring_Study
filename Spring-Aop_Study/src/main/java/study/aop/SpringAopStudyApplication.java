25c62f90fc147348b57f7a9019c66a0c9ac314bf
jeyong <joon6093@naver.com>
2024-08-14 00:42:50 +0900

    init : Spring Aop 공부를 위한 프로젝트 생성

---
+++ Spring-Aop_Study/src/main/java/study/aop/SpringAopStudyApplication.java
@@ -0,0 +1,13 @@
+package study.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopStudyApplication.class, args);
+    }
+
+}
---
+++ Spring-Aop_Study/src/test/java/study/aop/SpringAopStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
