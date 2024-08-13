2d34352afb089fc5d23128994855994fa0dfe224
jeyong <joon6093@naver.com>
2024-08-14 01:46:51 +0900

    feat : @Aspect를 이용한 package에 AOP 사용하기

--- Spring-Aop_Study/src/test/java/study/aop/AopTest.java
+++ Spring-Aop_Study/src/test/java/study/aop/AopTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import study.aop.order.OrderRepository;
 import study.aop.order.OrderService;
+import study.aop.order.aop.AspectV1;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
