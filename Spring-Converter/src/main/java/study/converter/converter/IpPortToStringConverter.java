b3d89d00a24ee523ed677490633116d09aaa7481
jeyong <joon6093@naver.com>
2024-08-15 05:32:42 +0900

    feat : converter 사용하기

---
+++ Spring-Converter/src/main/java/study/converter/converter/IpPortToStringConverter.java
@@ -0,0 +1,15 @@
+package study.converter.converter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import study.converter.type.IpPort;
+
+@Slf4j
+public class IpPortToStringConverter implements Converter<IpPort, String> {
+    @Override
+    public String convert(IpPort source) {
+        log.info("convert source={}", source);
+        //IpPort 객체 -> "127.0.0.1:8080"
+        return source.getIp() + ":" + source.getPort();
+    }
+}
