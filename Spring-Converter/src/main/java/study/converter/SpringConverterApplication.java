bd575981ddc3fae36bb87cac61255596401c09dd
jeyong <joon6093@naver.com>
2024-08-15 05:32:15 +0900

    refactor : 폴더 구조 변경

--- Spring-Converter/src/main/java/study/converter/SpringConverterApplication.java
+++ Spring-Converter/src/main/java/study/converter/SpringConverterApplication.java
@@ -1,4 +1,4 @@
-package study.converter.springconverter;
+package study.converter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Spring-Converter/src/test/java/study/converter/SpringConverterApplicationTests.java
+++ Spring-Converter/src/test/java/study/converter/SpringConverterApplicationTests.java
@@ -1,4 +1,4 @@
-package study.converter.springconverter;
+package study.converter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
