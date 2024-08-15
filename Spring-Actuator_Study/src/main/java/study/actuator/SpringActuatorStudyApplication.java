362e69bb6dc5243a8ba2d1e2b728f2ba69c8062a
jeyong <joon6093@naver.com>
2024-08-16 08:28:45 +0900

    feat : HttpExchange 사용하기

--- Spring-Actuator_Study/src/main/java/study/actuator/SpringActuatorStudyApplication.java
+++ Spring-Actuator_Study/src/main/java/study/actuator/SpringActuatorStudyApplication.java
@@ -1,7 +1,9 @@
 package study.actuator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringActuatorStudyApplication {
@@ -10,4 +12,8 @@ public class SpringActuatorStudyApplication {
         SpringApplication.run(SpringActuatorStudyApplication.class, args);
     }
 
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 }
