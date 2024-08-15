fa8314a44f85328d3ed81a82ceac27881269d0a4
jeyong <joon6093@naver.com>
2024-08-16 04:23:24 +0900

    feat : @Conditional 사용하기

---
+++ Spring-Boot_Study/src/main/java/study/boot/config/MemoryConfig.java
@@ -0,0 +1,24 @@
+package study.boot.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import study.memory.MemoryController;
+import study.memory.MemoryFinder;
+
+@Configuration
+//@Conditional(MemoryCondition.class)
+@ConditionalOnProperty(name = "memory", havingValue = "on")
+public class MemoryConfig {
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+
+}
