3903e67ad1d31071e63f1894f383e24972aed70a
jeyong <joon6093@naver.com>
2024-08-16 07:54:05 +0900

    init : Spring Actuator 공부를 위한 프로젝트 생성

---
+++ Spring-Actuator_Study/src/main/java/study/actuator/SpringActuatorStudyApplication.java
@@ -0,0 +1,13 @@
+package study.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringActuatorStudyApplication.class, args);
+    }
+
+}
---
+++ Spring-Actuator_Study/src/test/java/study/actuator/SpringActuatorStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
