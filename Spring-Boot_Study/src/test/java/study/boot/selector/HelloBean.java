8c8be9969c44ff4f3bd93e319dab5c11b7ab84b9
jeyong <joon6093@naver.com>
2024-08-16 04:56:09 +0900

    feat : ImportSelector 사용하기

---
+++ Spring-Boot_Study/src/test/java/study/boot/selector/HelloBean.java
@@ -0,0 +1,4 @@
+package study.boot.selector;
+
+public class HelloBean {
+}
---
+++ Spring-Boot_Study/src/test/java/study/boot/selector/HelloImportSelector.java
@@ -0,0 +1,11 @@
+package study.boot.selector;
+
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+public class HelloImportSelector implements ImportSelector {
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        return new String[]{"study.boot.selector.HelloConfig"};
+    }
+}
