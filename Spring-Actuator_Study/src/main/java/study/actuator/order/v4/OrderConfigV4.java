5ced5c5602a7a2819ba17286f41f5eb4dc8405d7
jeyong <joon6093@naver.com>
2024-08-17 02:50:17 +0900

    feat : @Timed를 사용해서 AOP를 이용하기

---
+++ Spring-Actuator_Study/src/main/java/study/actuator/order/v4/OrderConfigV4.java
@@ -0,0 +1,21 @@
+package study.actuator.order.v4;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import study.actuator.order.OrderService;
+
+@Configuration
+public class OrderConfigV4 {
+
+    @Bean
+    OrderService orderService() {
+        return new OrderServiceV4();
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
+}
