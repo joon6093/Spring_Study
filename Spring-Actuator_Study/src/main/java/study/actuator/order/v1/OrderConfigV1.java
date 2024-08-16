86fb3549820ad3e4eada8c894a9f96b9e31b1913
jeyong <joon6093@naver.com>
2024-08-17 02:28:26 +0900

    feat : MeterRegistry 사용해서 카운터 등록하기

---
+++ Spring-Actuator_Study/src/main/java/study/actuator/order/v1/OrderConfigV1.java
@@ -0,0 +1,15 @@
+package study.actuator.order.v1;
+
+import study.actuator.order.OrderService;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV1 {
+
+    @Bean
+    OrderService orderService(MeterRegistry registry) {
+        return new OrderServiceV1(registry);
+    }
+}
