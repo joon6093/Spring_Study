f7fd2702811db29e85d1ea7abff744530d31c2d1
jeyong <joon6093@naver.com>
2024-08-17 02:57:29 +0900

    feat : 게이지 등록하기

--- Spring-Actuator_Study/src/main/java/study/actuator/SpringActuatorStudyApplication.java
+++ Spring-Actuator_Study/src/main/java/study/actuator/SpringActuatorStudyApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeReposi
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+//@Import(OrderConfigV0.class)
+//@Import(OrderConfigV1.class)
+//@Import(OrderConfigV2.class)
+//@Import(OrderConfigV3.class)
+//@Import(OrderConfigV4.class)
+//@Import({OrderConfigV4.class, StockConfigV1.class})
+//@Import({OrderConfigV4.class, StockConfigV2.class})
+@SpringBootApplication(scanBasePackages = "study.actuator.controller")
 public class SpringActuatorStudyApplication {
 
     public static void main(String[] args) {
