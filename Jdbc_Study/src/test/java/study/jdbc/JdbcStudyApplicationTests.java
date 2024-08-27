3e8734cb5926c09bae80fabdc467784cdf716235
jeyong <joon6093@naver.com>
2024-08-27 18:29:41 +0900

    init : jdbc 공부를 위한 프로젝트 생성

---
+++ Jdbc_Study/src/test/java/study/jdbc/JdbcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
