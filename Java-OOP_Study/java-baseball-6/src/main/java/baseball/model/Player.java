1ce98bfab0e688b3b3980a70e9a080578ab9b423
jeyong <joon6093@naver.com>
2024-09-12 00:29:45 +0900

    refactor : model을 보호하기 위해 변하는 값을 주입받도록 변경

--- Java-OOP_Study/java-baseball-6/src/main/java/baseball/model/Player.java
+++ Java-OOP_Study/java-baseball-6/src/main/java/baseball/model/Player.java
@@ -1,7 +1,25 @@
 package baseball.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface Player {
-    List<Integer> getChosenNumbers();
+public class Player {
+    private final PlayerNumbers playerNumbers;
+
+    public Player(int input) {
+        this.playerNumbers = new PlayerNumbers(splitToDigits(input));
+    }
+
+    private List<Integer> splitToDigits(int number) {
+        List<Integer> digits = new ArrayList<>();
+        while (number > 0) {
+            digits.add(0, number % 10);
+            number /= 10;
+        }
+        return digits;
+    }
+
+    public List<Integer> getChosenNumbers() {
+        return playerNumbers.getNumbers();
+    }
 }
