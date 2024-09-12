dda2fd4df4cb7023375a9b29c2969fc6816a7fff
jeyong <118044367+joon6093@users.noreply.github.com>
2024-09-12 14:32:54 +0900

    feat : 도메인 기능을 위한 model 계층 구현

---
+++ Java-OOP_Study/java-racingcar-6/src/main/java/racingcar/model/Race.java
@@ -0,0 +1,38 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+    private final List<Car> cars;
+
+    public Race(List<String> carNames) {
+        cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void moveCars() {
+        for (Car car : cars) {
+            int randomValue = Randoms.pickNumberInRange(0, 9);
+            car.move(randomValue);
+        }
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+}
