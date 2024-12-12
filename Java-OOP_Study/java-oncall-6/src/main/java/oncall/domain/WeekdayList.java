b13fe5a5042900feb83bfda582525bd22c4c3e5d
jeyong <118044367+joon6093@users.noreply.github.com>
2024-12-12 16:36:43 +0900

    feat: 근무자 명단을 검증하고 저장하는 기능 구현

---
+++ Java-OOP_Study/java-oncall-6/src/main/java/oncall/domain/WeekdayList.java
@@ -0,0 +1,31 @@
+package oncall.domain;
+
+import java.util.List;
+import oncall.error.ErrorType;
+
+public class WeekdayList {
+
+    private final List<Worker> workers;
+
+    public WeekdayList(final List<Worker> workers) {
+        validateListSizeRange(workers);
+        validateUnique(workers);
+        this.workers = workers;
+    }
+
+    private void validateListSizeRange(final List<Worker> inputs) {
+        if (inputs.size() < 5 || inputs.size() > 35) {
+            throw new IllegalArgumentException(ErrorType.INVALID_INPUT.getMessage());
+        }
+    }
+
+    private void validateUnique(final List<Worker> inputs) {
+        if (inputs.stream().distinct().count() != inputs.size()) {
+            throw new IllegalArgumentException(ErrorType.INVALID_INPUT.getMessage());
+        }
+    }
+
+    public List<Worker> getWorkers() {
+        return workers;
+    }
+}
