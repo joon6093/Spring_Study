8932ca839e58a7629637a07a19439ae8fd7ac2bf
jeyong <joon6093@naver.com>
2025-08-16 20:21:13 +0900

    init: 프로젝트 초기 세팅

---
+++ Spring-Graceful_Study/src/main/java/study/graceful/springgraceful_study/SpringGracefulStudyApplication.java
@@ -0,0 +1,13 @@
+package study.graceful.springgraceful_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGracefulStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGracefulStudyApplication.class, args);
+    }
+
+}
